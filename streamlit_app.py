b58f416c73ccc878dd4c98ca37896bd1f511bf38
YUKTHA-KIPI <104973913+YUKTHA-KIPI@users.noreply.github.com>
2023-06-29 19:26:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import snowflake-connector-python
+import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
